ed729b3d9175e9c0a5764cf31469e70d6b930a51
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-09-11 14:09:12 -0700

    trying to set the width of the input rows to a %

--- src/components/InputRow.tsx
+++ src/components/InputRow.tsx
@@ -8,10 +8,10 @@ export const InputRow = ({row, handleRemoveRow, handleDataChange }) => {
         <tr className="input-row" id={row.inputId.toString()}>
             {/* <td>{row.inputId}</td> */}
             <td></td>
-            <td><Form.Control id={ALGO_INPUT_FIELDS.INPUT_NAME} type="text" placeholder="What is the input name?" onChange={handleDataChange} value={row.inputName}/></td>
-            <td><Form.Control id={ALGO_INPUT_FIELDS.INPUT_DESC} type="text" placeholder="Describe the input parameter" onChange={handleDataChange} value={row.inputDesc} /></td>
+            <td><Form.Control style={{ width: '70%' }} id={ALGO_INPUT_FIELDS.INPUT_NAME} type="text" placeholder="What is the input name?" onChange={handleDataChange} value={row.inputName}/></td>
+            <td><Form.Control style={{ width: '70%' }} id={ALGO_INPUT_FIELDS.INPUT_DESC} type="text" placeholder="Describe the input parameter" onChange={handleDataChange} value={row.inputDesc} /></td>
             <td><Form.Switch id={ALGO_INPUT_FIELDS.IS_REQUIRED} className="center-align" aria-label="required_input" onChange={handleDataChange} checked={row.isRequired} /></td>
-            <td><Form.Control id={ALGO_INPUT_FIELDS.INPUT_DEFAULT} type="text" placeholder="Default value" onChange={handleDataChange} value={row.inputDefault} /></td>
+            <td><Form.Control style={{ width: '50%' }} id={ALGO_INPUT_FIELDS.INPUT_DEFAULT} type="text" placeholder="Default value" onChange={handleDataChange} value={row.inputDefault} /></td>
             <td style={{verticalAlign: "middle"}}><span><BsFillXCircleFill className="danger-icon" id={row.inputId.toString()} onClick={() => handleRemoveRow(row.inputId.toString())}/></span></td>
         </tr>
     )
